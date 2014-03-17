d7dd9d1574e69402381409770d8d8501d7b6be3e
Stefan Otto <sotto.de@gmail.com>
2014-03-17 20:48:04 +0100

    Putting CLI class in it's file.

--- lib/feeder.rb
+++ lib/feeder.rb
@@ -1,37 +1,9 @@
-require 'yell'
-require 'singleton'
-
 require 'feeder/gmail_watch.rb'
 require 'feeder/raspberry_pi.rb'
 require 'feeder/configuration.rb'
+require 'feeder/cli.rb'
 
 module Feeder
-  class CLI
-    include Singleton
-
-    Yell.new name: Object do |l|
-      l.adapter STDOUT, level: [:debug, :error, :warn, :fatal]
-      l.adapter :file, 'feed.log', level: [:info], format: Yell.format( "%d: %m", "%Y-%m-%d %H:%M" )
-    end
-    Object.send :include, Yell::Loggable
-
-    def run
-      logger.debug "starting feeder .. Ada is waiting for feed"
-
-      @config = Configuration.instance
-      gmail_credentials = @config.gmail_credentials
-      @gmail = GmailWatch.new(gmail_credentials[:login], gmail_credentials[:password])
-      @rpi = RaspberryPi.new
-
-      while true
-        @gmail.login
-        @rpi.rotate_motor if @gmail.check_mail
-        @gmail.logout
-        sleep(1)
-      end
-    end
-
-  end
 
 end
 
---
+++ lib/feeder/cli.rb
@@ -0,0 +1,30 @@
+require 'yell'
+require 'singleton'
+
+module Feeder
+  class CLI
+    include Singleton
+
+    Yell.new name: Object do |l|
+      l.adapter STDOUT, level: [:debug, :error, :warn, :fatal]
+      l.adapter :file, 'feed.log', level: [:info], format: Yell.format( "%d: %m", "%Y-%m-%d %H:%M" )
+    end
+    Object.send :include, Yell::Loggable
+
+    def run
+      logger.debug "starting feeder .. Ada is waiting for feed"
+
+      @config = Configuration.instance
+      gmail_credentials = @config.gmail_credentials
+      @gmail = GmailWatch.new(gmail_credentials[:login], gmail_credentials[:password])
+      @rpi = RaspberryPi.new
+
+      while true
+        @gmail.login
+        @rpi.rotate_motor if @gmail.check_mail
+        @gmail.logout
+        sleep(1)
+      end
+    end
+  end
+end
